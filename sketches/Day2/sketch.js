9f67a6d4dbb1507fff0a82bbead8f67697055c9c
LeKagou <Daniel.rocha@ecal.ch>
2023-12-05 14:40:40 +0100

    02

--- sketches/Day2/sketch.js
+++ sketches/Day2/sketch.js
@@ -1,194 +1,205 @@
 import { SpringNumber } from "../../shared/spring.js"
-import { sendSequenceNextSignal } from "../../shared/sequenceRunner.js"
+
+let sceneSize;
+let objSize;
+let objSizeCircle;
+let stage = 0;
+let differentiel = 0;
+let mouseDragState = false;
+
+let figerTrait1 = false;
+let figerTrait2 = false;
 
 
-class Circle {
+const springCircle = new SpringNumber({
+	position: 500, // start position
+	frequency: 2, // oscillations per second (approximate)
+	halfLife: 0.10 // time until amplitude is halved
+})
 
-  constructor() {
+const springX = new SpringNumber({
+	position: window.innerWidth / 2, // start position
+	frequency: 2, // oscillations per second (approximate)
+	halfLife: 0.5 // time until amplitude is halved
+})
 
-    this.positionX = width / 2
-    this.positionY = height / 2
-    this.velocityX = 0
-    this.velocityY = 0
-    this.scaleSpring = new SpringNumber({
-      position: 1,
-      frequency: 4,
-      halfLife: .4
-    })
-    this.gravity = false
-    this.isGrabbed = false
-    this.grabOffsetX = 0
-    this.grabOffsetY = 0
-    this.radius = 1 // will be set later
-  }
+const springY = new SpringNumber({
+	position: window.innerHeight / 2, // start position
+	frequency: 2, // oscillations per second (approximate)
+	halfLife: 0.5 // time until amplitude is halved
+})
 
-  isOverlapping(x, y) {
-    return dist(this.positionX, this.positionY, x, y) < this.radius
-  }
-  draw() {
 
-    push()
-    translate(this.positionX, this.positionY)
-    scale(this.scaleSpring.position)
-    circle(0, 0, this.radius * 2, this.radius * 2)
-    pop()
-
-  }
-
-}
-
-class Triangle {
-
-  constructor() {
-
-    this.positionX = 0
-    this.positionY = 0
-    this.scaleSpring = new SpringNumber({
-      position: 0,
-      frequency: 3,
-      halfLife: .3
-    })
-    this.rotationSpring = new SpringNumber({
-      position: 0,
-      frequency: 1.5,
-      halfLife: .13,
-      wrap: 360
-    })
-
-    this.halfWidth = 1
-    this.height = 1
-  }
-  draw() {
-
-    push()
-    translate(this.positionX, this.positionY)
-    angleMode(DEGREES)
-    rotate(this.rotationSpring.position)
-    scale(this.scaleSpring.position)
-    triangle(
-      0, - this.height * 0.666,
-      this.halfWidth, this.height * 0.333,
-      - this.halfWidth, this.height * 0.333)
-
-    pop()
-  }
-}
-
-let sceneSize
-let objSize
-let myTriangle
-let myCircle
-let isFinished = false
 window.setup = function () {
   createCanvas(windowWidth, windowHeight);
-
-  myCircle = new Circle()
-  myCircle.positionX = width / 2
-  myCircle.positionY = height / 2
-
-  myTriangle = new Triangle()
-  myTriangle.positionX = width / 2
-  myTriangle.positionY = height / 2
-
+  sceneSize = min(width, height)
+  objSize = sceneSize / 2;
 }
 
 window.windowResized = function () {
   resizeCanvas(windowWidth, windowHeight);
 }
 
+window.mouseClicked = function (){
+  if(stage == 0){
+    stage = 1;
+    springCircle.target = 20;
+  }
+}
+
+let posMX = 0;
+let posMY = 0;;
+
+window.mouseDragged = function (){
+
+  if(stage == 2 || stage == 3)
+  {
+    mouseDragState = true;
+  }
+}
+
+window.mouseReleased = function (){ 
+
+  let d = dist(window.innerWidth / 2, window.innerHeight / 2, mouseX, mouseY);
+  
+  if(stage == 2 && d >= 300){
+    figerTrait1 = true;
+    springX.position = window.innerWidth / 2;
+    d = 0;
+  }
+  else if(stage == 3 && d >= 300){
+    figerTrait2 = true;
+    d = 0;
+  }
+
+  mouseDragState = false;
+}
+
 window.draw = function () {
+
+  background(255);
 
   const centerX = width / 2
   const centerY = height / 2
+  const strokeW = 20
   sceneSize = min(width, height)
-  objSize = sceneSize / 2
+  objSize = (sceneSize / 2);
 
-  myTriangle.halfWidth = objSize * 0.5773502692
-  myTriangle.height = objSize
+  springX.step(deltaTime / 1000) // deltaTime is in milliseconds, we need it in seconds
+  springY.step(deltaTime / 1000) // deltaTime is in milliseconds, we need it in seconds
+  springCircle.step(deltaTime / 1000) // deltaTime is in milliseconds, we need it in seconds
 
-  myCircle.radius = objSize * 0.5773502692 // magic number, keep it
+	const x = springX.position
+  const y = springY.position
+  const sCircle = springCircle.position;
 
-  if (!myCircle.isGrabbed) {
-    if (myCircle.isOverlapping(mouseX, mouseY) && mouseIsPressed) {
-      myCircle.isGrabbed = true
-      myCircle.grabOffsetX = (myCircle.positionX - mouseX) / myCircle.scaleSpring.position
-      myCircle.grabOffsetY = (myCircle.positionY - mouseY) / myCircle.scaleSpring.position
+  //cross(centerX,centerY,strokeW);
+
+  switch(stage){
+    case 0:
+      //Cercle Statique
+      circleShape(centerX,centerY);
+      break;
+    case 1:
+      //Cercle qui raptisse
+      circleShape(centerX,centerY);
+      setTimeout(function() {
+        stage = 2;
+      }, 1500);
+      break;
+    case 2:
+      //Déplacement des lignes Horizontales et Verticales
+      if(mouseDragState && !figerTrait1)
+      {
+        springY.target = mouseY
+      }
+      else if(!mouseDragState && !figerTrait1)
+      {
+        springY.target = centerY
+      }
+      if(figerTrait1)
+      {
+        springY.target = centerY + objSize / 2
+        setTimeout(function() {
+          stage = 3;
+        }, 1500);
+      }
+      push()
+      strokeWeight(strokeW);
+      line(centerX, centerY, centerX, y);
+      line(centerX, centerY, centerX, window.innerHeight - y);
+      pop()
+      circleShape(centerX,centerY);
+      break;
+      
+    case 3:{
+
+      console.log(x, springX.position, springX.target)
+
+
+      push()
+      strokeWeight(strokeW);
+      line(centerX, centerY - objSize / 2, centerX, centerY + objSize / 2)
+      pop()
+
+      if(mouseDragState && !figerTrait2)
+      {
+        springX.target = mouseX
+      }
+      else if(!mouseDragState && !figerTrait2)
+      {
+        springX.target = centerX
+      }
+      if(figerTrait2)
+      {
+        springX.target = centerX - objSize / 2
+        setTimeout(function() {
+          //stage = 4;
+        }, 1500);
+      }
+
+      push()
+      strokeWeight(strokeW);
+      line(centerX, centerY, x, centerY);
+      line(centerX, centerY, window.innerWidth - x, centerY);
+      pop()
+    }
+    case 4:{
+      /*push()
+      console.log(4);
+      strokeWeight(strokeW);
+      line(centerX - objSize / 2, centerY, centerX + objSize / 2, centerY)
+      line(centerX, centerY - objSize / 2, centerX, centerY + objSize / 2)
+      pop()*/
     }
   }
-  else {
-    if (!mouseIsPressed) {
-      myCircle.isGrabbed = false
-      myCircle.gravity = true
-    }
-  }
-  const dt = deltaTime / 1000
-  if (myCircle.isGrabbed) {
-    const lastX = myCircle.positionX
-    const lastY = myCircle.positionY
-    myCircle.positionX = mouseX + myCircle.grabOffsetX * myCircle.scaleSpring.position
-    myCircle.positionY = mouseY + myCircle.grabOffsetY * myCircle.scaleSpring.position
-    myCircle.velocityX = (myCircle.positionX - lastX) / dt
-    myCircle.velocityY = (myCircle.positionY - lastY) / dt
-
-
-  }
-
-  if (myCircle.gravity) {
-    const gravity = 2000
-    myCircle.velocityY += gravity * dt
-  }
-  if (!myCircle.isGrabbed) {
-    myCircle.positionX += myCircle.velocityX * dt
-    myCircle.positionY += myCircle.velocityY * dt
-  }
-
-  const d = dist(myCircle.positionX, myCircle.positionY, centerX, centerY)
-  const isFar = d > myCircle.radius * 2
-  const isBig = !myCircle.isGrabbed && isFar
-
-  myTriangle.scaleSpring.target = isBig ? 1 : 0.2
-
-  if (!isBig) {
-    const triangleToCircleX = myCircle.positionX - myTriangle.positionX
-    const triangleToCircleY = myCircle.positionY - myTriangle.positionY
-
-    myTriangle.rotationSpring.target = atan2(triangleToCircleY, triangleToCircleX) + 90
-  }
-  else {
-
-    myTriangle.rotationSpring.target = 0
-  }
-
-  const isOut = d > max(width / 2, height / 2) + myCircle.radius
-
-  const angleDiff = deltaAngle(myTriangle.rotationSpring.position, myTriangle.rotationSpring.target)
-  const rotationReachedTarget = abs(angleDiff) < 0.1
-  if (isOut && rotationReachedTarget && !isFinished) {
-    isFinished = true
-
-    sendSequenceNextSignal()
-  }
-
-  myTriangle.scaleSpring.step(dt)
-  myTriangle.rotationSpring.step(dt)
-
-  myCircle.scaleSpring.target = myCircle.isGrabbed ? .9 : 1
-  myCircle.scaleSpring.step(dt)
-
-  background(255);
-  fill(0)
-  noStroke()
-
-  myTriangle.draw()
-  myCircle.draw()
 
 }
 
-const deltaAngle = (a1, a2) => {
-  let diff = mod(a2 - a1, 360);
-  if (diff > 180)
-    diff -= 360;
-  return diff;
-};
+function circleShape(x,y){
 
-const mod = (n, m) => ((n % m) + m) % m;
+  objSizeCircle = (sceneSize / 2) - differentiel;
+
+  const sCircle = springCircle.position;
+
+  //Cercle
+  push()
+  fill(0,0,0)
+  noStroke()
+  translate(x, y);
+  circle(0, 0, sCircle)
+  pop()
+  //---
+}
+
+
+
+function cross(cX,cY,sW){
+  fill(255,0,0, 0.5)
+  noStroke()
+  rectMode(CENTER)
+  strokeWeight(sW)
+  stroke(0)
+  line(cX - objSize / 2, cY, cX + objSize / 2, cY)
+  line(cX, cY - objSize / 2, cX, cY + objSize / 2)
+}
